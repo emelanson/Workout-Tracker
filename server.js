3f279a6a80f2000910b2fd2ed8b935121ddfead8
emelanson <elijahmelanson@gmail.com>
2021-02-21 15:46:57 -0500

    Wrote first route, delivers index.js.

--- server.js
+++ server.js
@@ -20,6 +20,61 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { us
 
 app.use(routes)
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
+
+
+// db.Library.create({ name: "Campus Library" })
+//     .then(dbLibrary => {
+//         console.log(dbLibrary);
+//     })
+//     .catch(({ message }) => {
+//         console.log(message);
+//     });
+
+// app.post("/submit", ({body}, res) => {
+//   db.Book.create(body)
+//     .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/books", (req, res) => {
+//   db.Book.find({})
+//     .then(dbBook => {
+//       res.json(dbBook);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/library", (req, res) => {
+//   db.Library.find({})
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/populated", (req, res) => {
+//   db.Library.find({})
+//     .populate("books")
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
