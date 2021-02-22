bd9520f3a7cdcd6012dd14b2ad102b32617c6989
emelanson <elijahmelanson@gmail.com>
2021-02-21 21:23:05 -0500

    created virtual on model to compute totalDuration

--- models/Workout.js
+++ models/Workout.js
@@ -14,6 +14,14 @@ const WorkoutSchema = new Schema({
         duration: { type: Number },
         distance: { type: Number },
     }]
+}, //include computed property on JSON for stats
+    { toJSON: { virtuals: true } }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
 });
 
 //compile to model
